ba7a82f271e7cfdf7f416c5e2a6e1c3aef6e431c
Jack Shales <jackshalesmusic@gmail.com>
2016-07-07 21:12:03 -0500

    add individual item pages

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -3,5 +3,20 @@ class DrinksController < ApplicationController
     @drinks = Drink.all
     render 'home.html.erb'
   end
+
+  def cold_brew
+    @cold_brew = Drink.first
+    render 'cold_brew.html.erb'
+  end
+
+  def decaf_cold_brew
+    @decaf_cold_brew = Drink.second
+    render 'decaf_cold_brew.html.erb'
+  end
+
+  def black_tea
+    @black_tea = Drink.third
+    render 'black_tea.html.erb'
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/home' => 'drinks#all_products'
+  get '/cold_brew' => 'drinks#cold_brew'
+  get '/decaf_cold_brew' => 'drinks#decaf_cold_brew'
+  get '/black_tea' => 'drinks#black_tea'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
